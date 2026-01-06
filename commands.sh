a04d84d12701f3eab7157d476a13ed2f09867ccf
William L. Thomson Jr. <w@wltjr.com>
2026-01-06 14:41:17 -0500

    commands.sh: Remove left over echo statements, poor Q/A...

--- commands.sh
+++ commands.sh
@@ -10,8 +10,8 @@ echo 'export GZ_SIM_RESOURCE_PATH=${GAZEBO_MODEL_PATH}:${GZ_SIM_RESOURCE_PATH}' 
 	>> /root/.bashrc
 
 cat >> /root/.bash_history<< EOF
-echo ros2 launch turbopi_ros gamepad.launch.py
-echo ros2 launch turbopi_ros ign_gazebo.launch.py custom:=False world:=playground
+ros2 launch turbopi_ros gamepad.launch.py
+ros2 launch turbopi_ros ign_gazebo.launch.py custom:=False world:=playground
 ros2 launch turbopi_ros ign_gazebo.launch.py
 ros2 launch turbopi_ros nav2.launch.py
 rviz2 -d src/turbopi_ros/config/turbopi.rviz
