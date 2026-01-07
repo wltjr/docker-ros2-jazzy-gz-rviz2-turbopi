8f6244ba1af2068a1a90fca2e91b4029c6e04723
William L. Thomson Jr. <w@wltjr.com>
2026-01-06 19:23:25 -0500

    commands.sh: Update rm command to remove only turbopi_ros from build dir

--- commands.sh
+++ commands.sh
@@ -15,5 +15,5 @@ ros2 launch turbopi_ros ign_gazebo.launch.py custom:=False world:=playground
 ros2 launch turbopi_ros ign_gazebo.launch.py
 ros2 launch turbopi_ros nav2.launch.py
 rviz2 -d src/turbopi_ros/config/turbopi.rviz
-rm -r build; colcon build --symlink-install --packages-select  turbopi_ros
+rm -r build/turbopi_ros/; colcon build --symlink-install --packages-select  turbopi_ros
 EOF
