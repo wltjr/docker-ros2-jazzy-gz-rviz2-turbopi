7c2e2bfd0a6e51c744966af6a43b09c667b6ac17
William L. Thomson Jr. <w@wltjr.com>
2026-03-03 10:11:21 -0500

    commands.sh: Correct/update command with arguments

--- commands.sh
+++ commands.sh
@@ -11,7 +11,7 @@ echo 'export GZ_SIM_RESOURCE_PATH=${GAZEBO_MODEL_PATH}:${GZ_SIM_RESOURCE_PATH}' 
 
 cat >> /root/.bash_history<< EOF
 ros2 launch turbopi_ros gamepad.launch.py
-ros2 launch turbopi_ros ign_gazebo.launch.py custom:=False world:=playground
+ros2 launch turbopi_ros ign_gazebo.launch.py depth:=True drive:=mecanum lidar:=True world:=playground
 ros2 launch turbopi_ros ign_gazebo.launch.py
 ros2 launch turbopi_ros nav2.launch.py
 rviz2 -d src/turbopi_ros/config/turbopi.rviz
